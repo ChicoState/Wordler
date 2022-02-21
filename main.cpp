7992db8a40bcacbcaa6e852e07d0e4393842a08f
Meher Lippmann <melippmann@mail.csuchico.edu>
2022-02-21 11:37:50 -0800

    added functionality so that if user types quit the program will terminate

--- main.cpp
+++ main.cpp
@@ -26,8 +26,13 @@ int main(){
         // require user to enter another guess if their word isn't 5 letters long
         do{
             std::cin >> guess;
+            if(guess == "quit"){
+                return 0;
+            }
         }while( guess.length() != 5 );
-
+            if(guess == "quit"){
+                return 0;
+            }
         // capitalize guess for easy comparisons
         capitalize(guess);
         guesses++;
