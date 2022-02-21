e8161c8b94282e5269fa1da1d9dcc9ee5fc8cc87
jvi1 <98780269+jvi1@users.noreply.github.com>
2022-02-21 11:50:24 -0800

    resolves #2 fixed the function capitalize

--- main.cpp
+++ main.cpp
@@ -63,7 +63,7 @@ std::string get_hint(std::string match, std::string word){
 }
 
 // capitalizes a word (to UPPER CASE)
-void capitalize(std::string & word){
+void capitalize(std::string &word){
     for(int i=0; i<word.length(); i++){
         word[i] = toupper(word[i]);
     }
