eb0c8f014d16665eab849ec5ca237935d032287d
ydoco <cwillard1@mail.csuchico.edu>
2022-02-21 11:12:17 -0800

    added quit feature, resolves #2, also added worlflow resolves #1

--- main.cpp
+++ main.cpp
@@ -24,6 +24,10 @@ int main(){
     do{
         do{
             std::cin >> guess;
+            if(guess == "quit"){
+                std::cout << "Quitting the game" << std::endl;
+                return 1;
+            }
         }while( guess.length() != 5 );
 
         // capitalize guess for easy comparisons
