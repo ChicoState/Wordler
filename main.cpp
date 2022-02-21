9ebc30cee154bc8cea5e7f65b4fd3aba726b1f5d
lokeshkumaraguru <lkumaraguru@mail.csuchico.edu>
2022-02-21 11:18:16 -0800

    q01 changes.

--- main.cpp
+++ main.cpp
@@ -24,6 +24,11 @@ int main(){
     do{
         do{
             std::cin >> guess;
+
+            if (guess == "quit") {
+                    exit(0);
+            }
+
         }while( guess.length() != 5 );
 
         // capitalize guess for easy comparisons
