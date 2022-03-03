eb3aa2fda2bffc9346e5e0e22f84297bfd86fece
Cole-Hop <chopkins1@csuchico.edu>
2022-03-03 14:16:17 -0800

    fixed std issue

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ int main()
 
             if(guess ==  "quit")
             {
-                std::cout << "Quitting Program..." << endl;
+                std::cout << "Quitting Program..." << std::endl;
                 exit(1);
             }
 
