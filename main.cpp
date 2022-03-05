f53cd9f29fc7e13c1b587743575e2532929176e9
jarchuleta530 <87887495+jarchuleta530@users.noreply.github.com>
2022-03-04 16:15:50 -0800

    Add quit function and fixed capitalization

--- main.cpp
+++ main.cpp
@@ -25,11 +25,10 @@ int main(){
         // require user to enter another guess if their word isn't 5 letters long	
 	    do{
             	std::cin >> guess;
-        	
 	        if (guess == "quit"){
+		std::cout << "Now quitting";
 	        exit(1);
 		}
-
 	    }while( guess.length() != 5 );
         // capitalize guess for easy comparisons
         guess = capitalize(guess);
